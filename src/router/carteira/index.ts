c1b52a44ffd73e468646342d197c90ade2294cfc
DESKTOP-SH3SALR\Wetalk-PC <gustavo@wetalkit.com.br>
2025-06-27 11:09:21 -0300

    adição da parte de ver carteira

--- src/router/carteira/index.ts
+++ src/router/carteira/index.ts
@@ -4,45 +4,52 @@ import prisma from "../../pisma";
 import {
   extractUserDataFromToken,
   validateJWT,
-} from '../../middlewares/JWTverifier';;
+} from "../../middlewares/JWTverifier";
 
 export const routerCarteira = Router();
 
-// Cria uma nova carteira enviando: { name: string; icon: string }
-routerCarteira.post(
-  "/add",
-  validateJWT,
-  async (req, res) => {
-    // 1) extrai dados do usuário
-   const userData = await extractUserDataFromToken(req, res);
-    if (!userData || "status" in userData) {
-      return;
-    }
-
-    // 2) lê name e icon do body
-    const { name, icon } = req.body as { name: string; icon: string };
-
-    if (typeof name !== "string" || typeof icon !== "string") {
-       res
-        .status(400)
-        .json({ message: "Campos name e icon devem ser strings" });
-    }
-
-    // 3) cria no banco
-    try {
-      const newPortfolio = await prisma.portfolio.create({
-        data: {
-          user_id: userData.id,
-          nome_carteira: name,
-          icon: icon,  // grava o nome do AntIcon, ex: "HomeOutlined"
-        },
-      });
-       res.status(201).json(newPortfolio);
-    } catch (error) {
-      console.error("Erro ao criar portfolio:", error);
-       res
-        .status(500)
-        .json({ message: "Erro interno ao criar carteira" });
-    }
+routerCarteira.post("/add", validateJWT, async (req, res) => {
+  const userData = await extractUserDataFromToken(req, res);
+  if (!userData || "status" in userData) {
+    return;
   }
-);
+
+  const { name, icon } = req.body as { name: string; icon: string };
+
+  if (typeof name !== "string" || typeof icon !== "string") {
+    res.status(400).json({ message: "Campos name e icon devem ser strings" });
+  }
+
+  try {
+    const newPortfolio = await prisma.portfolio.create({
+      data: {
+        user_id: userData.id,
+        nome_carteira: name,
+        icon: icon,
+      },
+    });
+    res.status(201).json(newPortfolio);
+  } catch (error) {
+    console.error("Erro ao criar portfolio:", error);
+    res.status(500).json({ message: "Erro interno ao criar carteira" });
+  }
+});
+
+routerCarteira.get("/list", validateJWT, async (req, res) => {
+  const userData = await extractUserDataFromToken(req, res);
+  if (!userData || "status" in userData) {
+    return;
+  }
+
+  try {
+    const findPortfolio = await prisma.portfolio.findMany({
+      where: {
+        user_id: userData.id,
+      },
+    });
+    res.status(201).json(findPortfolio);
+  } catch (error) {
+    console.error("Erro ao criar portfolio:", error);
+    res.status(500).json({ message: "Erro interno ao criar carteira" });
+  }
+});
